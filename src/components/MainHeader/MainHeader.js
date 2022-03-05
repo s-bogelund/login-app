20727c97f85df95a081592c5545225b3f580102b
s-bogelund <simbj94@gmail.com>
2022-03-05 18:15:59 +0100

    forgot that I didn`t care about the styling, so I changed the colors around and some other stuff and I think I inadverdently made it look worse

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -6,7 +6,8 @@ import classes from './MainHeader.module.css';
 const MainHeader = (props) => {
   return (
     <header className={classes['main-header']}>
-      <h1>A Typical Page</h1>
+      <h1>The Best Login Page</h1>
+      <h3>We will NEVER store your user data!! (cause we don`t know how yet)</h3>
       <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} />
     </header>
   );
